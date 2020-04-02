dad48a304c2974e871313f6c9eeecd1d1307508f
Graham Ollis <plicease@cpan.org>
2020-04-02 11:18:56 -0600

    Initial structure

---
+++ lib/Alien/wasmtime.pm
@@ -0,0 +1,13 @@
+package Alien::wasmtime;
+
+use strict;
+use warnings;
+use 5.008001;
+use base qw( Alien::Base );
+
+# ABSTRACT: Find or download wasmtime for use by other Perl modules
+# VERSION
+
+1;
+
+
